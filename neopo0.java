dd70cde55bd933e84d4f89e91848d8cc9de82eb0
Parthipanbhara <41136495+Parthipanbhara@users.noreply.github.com>
2018-07-12 12:28:07 +0530

    Add files via upload

---
+++ neopo0.java
@@ -0,0 +1,24 @@
+package partha;
+import java.util.Scanner;
+public class neopo0 {
+	public static void main(String[] args) {
+		int a;
+		Scanner sp=new Scanner(System.in);
+		a=sp.nextInt();
+		if(a!=0){
+			if(a>0){
+				System.out.println("positive");
+			}
+			else if(a<0){
+				System.out.println("negative");
+			}}
+			else
+			{
+				System.out.println("zero");
+			}
+		
+			
+		
+	}
+
+}
